ceeccfc2cd7e3aea16a3f3f58f1e0e37de90034a
AJ Keeter <aleckeeter94@gmail.com>
2019-06-16 12:24:57 -0500

    Created some data for groups and contacts table using database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.destroy_all
+Group.destroy_all
+
+
+group_ids = []
+# both of these methods below work
+group_ids.push(Group.create(name: "Client").id)
+group_ids << Group.create(name: "Ass n Titties").id
+group_ids << Group.create(name: "C U Next Tuesday").id
+group_ids << Group.create(name: "fack bruh").id
+
+p "#{group_ids.count} created"
+
+group_count = group_ids.length
+number_of_contacts = 20
+contacts = []
+
+number_of_contacts.times do |i|
+  new_contact = {
+      name: Faker::Name.name,
+      name: Faker::Internet.email,
+      company: Faker::Company.name,
+      phone: Faker::PhoneNumber.cell_phone,
+      address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city}",
+      group_id: group_ids[Random.rand(0...group_count)]
+  }
+  contacts.push(new_contact)
+
+end
+
+Contact.create(contacts)
+
+p "#{number_of_contacts} contacts created"
